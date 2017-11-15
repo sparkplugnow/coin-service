d2b05d2f4f871e34517e3f7dfb23c3c95c7f9943
Alexander <alex.nnakwue@gmail.com>
2017-11-15 04:43:47 +0100

    added the leaderboard route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+
 const Transaction =  require('../models/Transaction.js');
 const Wallet = require('../models/Transaction.js');
 const creds = require('../creds');
@@ -8,5 +9,25 @@ const creds = require('../creds');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Coin Service' });
 });
+
+router.get('/', function (req, res, next) {
+  User
+    .find({}, function (err, users) {
+      if (err) 
+        throw err;
+      Wallet
+        .find({})
+        .sort('-balance')
+        .exec(function (err, wallets) {
+          if (err) 
+            throw err;
+          res.render('index', {
+            users: users,
+            wallets: wallets
+          })
+        })
+    })
+  })
+
 
 module.exports = router;
