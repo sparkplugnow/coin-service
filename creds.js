d2f929daad3ee6bd69093678f416cbefc1adcd1e
Israel Adura <israeladura@gmail.com>
2017-11-24 11:49:46 +0100

    Remove creds file

--- creds.js
+++
@@ -1,8 +0,0 @@
-module.exports.creds = {
-  mongoDb : 'mongodb://crudexec:Fibonacci1234@ds259245.mlab.com:59245/coinservice'
-}
-
-module.exports.token = {
-  token: 'xoxb-268367665959-Obm72SEmiXEYjdFTxq1Gg7Xw'
-}
-
